0325cdd67681aeb824c7449393a2dac4d91d02c6
wquerales <williams.querales@evertecinc.com>
2023-01-16 17:50:28 -0300

    configuration initial projects Api y WebUI

---
+++ src/Api/Models/StoreContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RiskCenterStoreApi.Models
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options) {
+        
+        }
+    }
+}
---
+++ src/Api/Program.cs
@@ -0,0 +1,33 @@
+using Microsoft.EntityFrameworkCore;
+using RiskCenterStoreApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Configure connection to db
+// Obtain the conection string
+var ConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+// Set context with conection to SQLServer
+builder.Services.AddDbContext<StoreContext>(options => options.UseSqlServer(ConnectionString));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
